e64b5b1d16acb76d6e5a213154c1587d64d442d7
Hunkarbot <hunkarveli33@gmail.com>
2026-03-22 00:09:09 +0100

    Fix bot

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
-import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-BOT_TOKEN = "8747036915:AAG9c4MRd6Fx-EDQOCpcxmFNGdRCAu995GE"
+BOT_TOKEN = "8747036915:"AAG9c4MRd6Fx-EDQOCpcxmFNGdRCAu995GE"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot aktif kral ✅")
+    await update.message.reply_text("Bot aktif ✅")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot çalışıyor...")
-app.run_polling()
+    print("Bot çalışıyor...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
