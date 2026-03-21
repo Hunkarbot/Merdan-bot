0ddb6f57077f20050c7156e15d6380dc37a6451f
Hunkarbot <hunkarveli33@gmail.com>
2026-03-21 22:05:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,11 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+token = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
     await update.message.reply_text("Bot aktif kral ✅")
 
-def main():
-    app=
-ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(token).build()
 app.add_handler(CommandHandler("start", start))
-print("Bot çalışıyor...")
 app.run_polling()
-
-if __name__ == "__main__":
-   main() 
