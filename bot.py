19b31470576612b2a5227d333d20deeb062b4479
Hunkarbot <hunkarveli33@gmail.com>
2026-03-21 22:58:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-token = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot aktif kral ✅")
 
-app = ApplicationBuilder().token(token).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+
+print("Bot çalışıyor...")
 app.run_polling()
