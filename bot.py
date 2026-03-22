71d537fa3b1a8141fde8043a976b6e311dc10339
Hunkarbot <hunkarveli33@gmail.com>
2026-03-22 08:54:37 +0100

    fix bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-BOT_TOKEN = "8747036915:"AAG9c4MRd6Fx-EDQOCpcxmFNGdRCAu995GE"
+BOT_TOKEN = "8747036915:AAG9c4MRd6Fx-EDQOCpcxmFNGdRCAu995GE"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot aktif ✅")
