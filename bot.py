571d6b6d48cbb4b79f6212f3f90004dc0df4db8c
Hunkarbot <hunkarveli33@gmail.com>
2026-03-22 09:51:09 +0100

    Fix  token

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-import os
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+BOT_TOKEN = "8747036915:AAG9c4MRd6Fx-EDQOCpcxmFNGdRCAu995GE"
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot aktif ✅")
-    async def mesaj(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot aktif 👑")
+
+async def mesaj(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Çalışıyorum 👍")
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mesaj))
+
 print("Bot çalışıyor...")
-app.run_polling() 
+app.run_polling()
