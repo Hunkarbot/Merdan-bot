86fb5f2211bc8087cad5f6165666cfcb2c443761
Hunkarbot <hunkarveli33@gmail.com>
2026-03-28 12:55:31 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,16 @@ import requests
 import os
 
 API_KEY = os.getenv("FOOTBALL_API_KEY")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
-print("API_KEY:", API_KEY)
-print("BOT_TOKEN var mı:", bool(BOT_TOKEN))
-print("CHAT_ID var mı:", bool(CHAT_ID))
+BASE_URL = "https://api.football-data.org/v4"
 
-url = "https://v3.football.api-sports.io/status"
-headers = {
-    "x-apisports-key": API_KEY
+HEADERS = {
+    "X-Auth-Token": API_KEY
 }
 
-response = requests.get(url, headers=headers)
+url = f"{BASE_URL}/matches"
 
-print("STATUS CODE:", response.status_code)
-print("RESPONSE:", response.text)
+res = requests.get(url, headers=HEADERS)
+
+print("STATUS:", res.status_code)
+print("DATA:", res.text[:500])
