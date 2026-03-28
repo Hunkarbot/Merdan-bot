581c29bf46739a23e34d8590b284776e4d0818a8
Hunkarbot <hunkarveli33@gmail.com>
2026-03-28 12:47:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@ import requests
 import os
 
 API_KEY = os.getenv("FOOTBALL_API_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+print("API_KEY:", API_KEY)
+print("BOT_TOKEN var mı:", bool(BOT_TOKEN))
+print("CHAT_ID var mı:", bool(CHAT_ID))
 
 url = "https://v3.football.api-sports.io/status"
-
 headers = {
     "x-apisports-key": API_KEY
 }
-
-print("API_KEY:", API_KEY)
-print("VAR MI:", bool(API_KEY))
 
 response = requests.get(url, headers=headers)
 
